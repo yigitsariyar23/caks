c7a5f310e6a41e5ada77db7add3a9192e0bbddbf
Ahmet Kerem Şaylı <a.keremsayli@gmail.com>
2026-06-22 14:20:25 +0300

    deploy fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
-import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/shared/theme-provider"
-
-const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "CAKS: City Attraction Kitchen Stay",
@@ -18,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className} suppressHydrationWarning>
+      <body suppressHydrationWarning>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
         </ThemeProvider>
@@ -26,4 +23,3 @@ export default function RootLayout({
     </html>
   )
 }
-
